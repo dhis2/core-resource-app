38fda137ec2af182907d437561ef389b20da9737
Mark Polak <mark@thedutchies.com>
2016-08-25 13:39:45 +0200

    Remove unused files

--- lodash/install.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash/fp');
-const lodashVersions = require('../libraries');
-const { install, getVersionsToInstall } = require('../utils/installer');
-
-install('lodash', '../build', getVersionsToInstall('lodash', _.get('js.lodash', lodashVersions)));
--- rxjs/install.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash/fp');
-const lodashVersions = require('../libraries');
-const { install, getVersionsToInstall } = require('../utils/installer');
-
-install('rxjs', '../build', getVersionsToInstall('rxjs', _.get('js.rxjs', lodashVersions)));
