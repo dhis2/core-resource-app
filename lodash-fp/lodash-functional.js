f383cbdcef2e2951d609b751a0842011bead46bc
Mark Polak <mark@thedutchies.com>
2016-10-12 18:31:14 +0200

    Don't export fp but set directly on the window

--- lodash-fp/lodash-functional.js
+++ lodash-fp/lodash-functional.js
@@ -1,3 +1,17 @@
 import baseConvert from 'lodash/fp/_baseConvert';
 
-module.exports = baseConvert(window._, window._, {});
+/**
+ * Converts `lodash` to an immutable auto-curried iteratee-first data-last
+ * version with conversion `options` applied.
+ *
+ * @param {Function} lodash The lodash function to convert.
+ * @param {Object} [options] The options object. See `baseConvert` for more details.
+ * @returns {Function} Returns the converted `lodash`.
+ */
+function browserConvert(lodash, options) {
+    return baseConvert(lodash, lodash, options);
+}
+
+if (typeof _ == 'function' && typeof _.runInContext == 'function') {
+    window.fp = browserConvert(_.runInContext());
+}
--- lodash-fp/webpack.config.js
+++ lodash-fp/webpack.config.js
@@ -4,6 +4,5 @@ module.exports = createWebpackConfig(__dirname)
     .withEntry('./lodash-functional.js')
     .withOutput({
         filename: 'lodash-functional/lodash-functional.js',
-        library: 'fp',
     })
     .build();
